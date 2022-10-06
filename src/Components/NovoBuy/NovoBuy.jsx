d572f346a064158a5a62b01cc62a1d36d5a8d69a
BenjiChejtman <46895375@est.ort.edu.ar>
2022-10-05 22:34:31 -0300

    .

--- src/Components/NovoBuy/NovoBuy.jsx
+++ src/Components/NovoBuy/NovoBuy.jsx
@@ -97,119 +97,8 @@ import React from "react";
    import { FaFileCsv } from "react-icons/fa";
    import MaterialTable from 'material-table';
 
-const Users = [
-  {
-    id: 1,
-    selected: false,
-    Board: "BC548",
-    CODE: "1727-2638-1-ND",
-    Quantity: "56",
-    StockAvailable: "OK",
-    Price: "$254"
-  },
-  {
-    id: 2,
-    selected: false,
-    Board: "BC548",
-    CODE: "",
-    Quantity: "1-770-736-8031 x56442",
-    StockAvailable: "hildegard.org",
-    Price: "$254"
-  },
-  {
-    id: 3,
-    selected: false,
-    Board: "BC548",
-    CODE: "",
-    Quantity: "1-770-736-8031 x56442",
-    StockAvailable: "hildegard.org",
-    Price: "$254"
-  },
-  {
-    id: 4,
-    selected: false,
-    Board: "BC548",
-    CODE: "",
-    Quantity: "1-770-736-8031 x56442",
-    StockAvailable: "hildegard.org",
-    Price: "$254"
-  },
-  {
-    id: 5,
-    selected: false,
-    Board: "BC548",
-    CODE: "",
-    Quantity: "1-770-736-8031 x56442",
-    StockAvailable: "hildegard.org",
-    Price: "$254"
-  },
-];
-
-const columns = [
-  { title: "Board", field: "id"},
-  { title: "Code", field: "code"},
-  { title: "Quantity", field: "quantity"},
-  { title: "Stock available", field: "stock available"},
-  { title: "Price", field: "price"},
-]
-
-class SelectTableComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      List: Users,
-      MasterChecked: false,
-      SelectedList: [],
-    };
-  }
-
-  // Select/ UnSelect Table rows
-  onMasterCheck(e) {
-    let tempList = this.state.List;
-    // Check/ UnCheck All Items
-    tempList.map((user) => (user.selected = e.target.checked));
-
-    //Update State
-    this.setState({
-      MasterChecked: e.target.checked,
-      List: tempList,
-      SelectedList: this.state.List.filter((e) => e.selected),
-    });
-  }
-
+   const NovoBuy = () => {
   
-  // Update List Item's state and Master Checkbox State
-  onItemCheck(e, item) {
-    let tempList = this.state.List;
-    tempList.map((user) => {
-      if (user.id === item.id) {
-        user.selected = e.target.checked;
-      }
-      return user;
-    });
-
-    //To Control Master Checkbox State
-    const totalItems = this.state.List.length;
-    const totalCheckedItems = tempList.filter((e) => e.selected).length;
-    // Update State
-    this.setState({
-      MasterChecked: totalItems === totalCheckedItems,
-      List: tempList,
-      SelectedList: this.state.List.filter((e) => e.selected),
-    });
-  }
-
-
-
-  // Event to get selected rows(Optional)
-  getSelectedRows() {
-    this.setState({
-      SelectedList: this.state.List.filter((e) => e.selected),
-    });
-  }
-
-  render() {
-    console.log("hola")
     return (
       <div className ="completeDiv2">
          <header className="DarkBar" id="DarkBar">
@@ -225,24 +114,24 @@ class SelectTableComponent extends React.Component {
                  </Button>
              </Link>
         </header>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className = "tableDiv">
               <MaterialTable
-                title="Novo parts"
-                data={Users}
-                columns={columns}
+                title="Basic Selection Preview"
+                columns={[
+                    { title: "Board", field: "board"},
+                    { title: "Code", field: "code"},
+                    { title: "Quantity", field: "quantity", type: "numeric"},
+                    { title: "Stock", field: "stock"},
+                    { title: "Price", field: "price", type: "numeric"},
+                ]}
+                data={[
+                    { board: 'Mehmet', code: 'Baran', quantity: 1987, stock: 63, price: 210 },
+                ]}
                 options={{
                   selection: true
-                }} />
-          </div>
+                }} 
+              />
         </div>
-      </div>
-    </div>
-  </div>
     );
-  }
-}
+};
 
-export default SelectTableComponent;
+export default NovoBuy;
